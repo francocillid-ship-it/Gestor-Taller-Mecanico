da85367fb84ce7f298d971e0bdb552808a9e3ca4
efecede <franco.cillid@gmail.com>
2026-02-16 09:43:27 -0300

    Remove unused CSS import from `index.tsx`.

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
 const isTextInput = (target: EventTarget | null) => {
     if (!(target instanceof HTMLElement)) return false;
